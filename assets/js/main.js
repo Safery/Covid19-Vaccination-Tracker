dd8686381107a513354bc39be96f14c35fcc7a24
janingpower <janingpower@yahoo.com>
2020-04-14 20:29:28 -0400

    Added map feature, removed cdc news and added sharing option for social media

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,8 @@
 */
 (function($) {
 
+    var arrayForMap = [];
+
     var $window = $(window),
         $body = $('body'),
         $main = $('#main');
@@ -137,6 +139,7 @@
     });
 
     // Read CDC article and get latest data
+    /**
     fetch("https://tools.cdc.gov/api/v2/resources/media/132608.rss")
         .then(response => response.text())
         .then(str => new window.DOMParser().parseFromString(str, "text/xml"))
@@ -153,13 +156,14 @@
                 }
             });
         });
+        */
 
     // Fetch Vaccine details for phase 2
     $.ajax({
-        url: "https://spreadsheets.google.com/feeds/cells/1yvmeAtpxonqmJ7OlO2gRBt2lUVnk3baeIHFu0M9ZLxo/1/public/values?alt=json",
+        url: "https://spreadsheets.google.com/feeds/cells/1s0j7roYF8zxqK-gNgqnKtkY8OrgmovzDwtjAc6LI3c0/1/public/values?alt=json",
         success: function(result) {
             var table = "";
-            for (var i = 0; i < result["feed"]["entry"].length - 1; i = i + 5) {
+            for (var i = 0; i < result["feed"]["entry"].length - 1; i = i + 6) {
                 table += "<tr>";
                 table += '<td style="font-size: 0.8em;">' + result["feed"]["entry"][i]["content"]["$t"] + '</td>';
                 table += '<td style="font-size: 0.8em;">' + result["feed"]["entry"][i + 1]["content"]["$t"] + '</td>';
@@ -167,57 +171,175 @@
                 table += '<td style="font-size: 0.8em;">' + result["feed"]["entry"][i + 3]["content"]["$t"] + '</td>';
                 var res = "<td>";
                 var totalSources = result["feed"]["entry"][i + 4]["content"]["$t"].split("|");
+                var varListOfLocation = result["feed"]["entry"][i + 5]["content"]["$t"].split("@");
+                for (var z=0; z < varListOfLocation.length; z++){
+                    arrayForMap.push({
+                        "title": result["feed"]["entry"][i]["content"]["$t"],
+                        "latitude": parseFloat((varListOfLocation[z].split(","))[0]),
+                        "longitude": parseFloat((varListOfLocation[z].split(","))[1]),
+                        "color":null
+                      });
+                }
                 for (var z = 0; z < totalSources.length; z++) {
                     res += '<a rel="external nofollow" href="' + totalSources[z] + '">Source[' + (z + 1) + ']</a><br>';
                 }
                 table += res + '</td>' + '</tr>';
             }
             document.getElementById("insertPhase2Here").innerHTML = table;
+            doPhase1();
         }
     });
 
     // Fetch Vaccine details for phase 1
-    $.ajax({
-        url: "https://spreadsheets.google.com/feeds/cells/1Ebxlw5HEqzr1JEO4atHZiYGweUUQ6gIPBECDjuvkHR8/1/public/values?alt=json",
-        success: function(result) {
-            var table = "";
-            for (var i = 0; i < result["feed"]["entry"].length - 1; i = i + 5) {
+    function doPhase1(){
+        $.ajax({
+            url: "https://spreadsheets.google.com/feeds/cells/1aca-atk3B4pK9GYya1UrBKWjFoA6Tn1_se61EEE2IUw/1/public/values?alt=json",
+            success: function(result) {
+                var table = "";
+                for (var i = 0; i < result["feed"]["entry"].length - 1; i = i + 6) {
 
-                table += "<tr>";
-                table += '<td style="font-size: 0.8em;">' + result["feed"]["entry"][i]["content"]["$t"] + '</td>';
-                table += '<td style="font-size: 0.8em;">' + result["feed"]["entry"][i + 1]["content"]["$t"] + '</td>';
-                table += '<td style="font-size: 0.8em;background-color:#99B898;color: white;" class="disMobile" >' + result["feed"]["entry"][i + 2]["content"]["$t"] + '</td>';
-                table += '<td style="font-size: 0.8em;">' + result["feed"]["entry"][i + 3]["content"]["$t"] + '</td>';
-                var res = "<td>";
-                var totalSources = result["feed"]["entry"][i + 4]["content"]["$t"].split("|");
-                for (var z = 0; z < totalSources.length; z++) {
-                    res += '<a rel="external nofollow" href="' + totalSources[z] + '">Source[' + (z + 1) + ']</a><br>';
+                    table += "<tr>";
+                    table += '<td style="font-size: 0.8em;">' + result["feed"]["entry"][i]["content"]["$t"] + '</td>';
+                    table += '<td style="font-size: 0.8em;">' + result["feed"]["entry"][i + 1]["content"]["$t"] + '</td>';
+                    table += '<td style="font-size: 0.8em;background-color:#99B898;color: white;" class="disMobile" >' + result["feed"]["entry"][i + 2]["content"]["$t"] + '</td>';
+                    table += '<td style="font-size: 0.8em;">' + result["feed"]["entry"][i + 3]["content"]["$t"] + '</td>';
+                    var res = "<td>";
+                    var totalSources = result["feed"]["entry"][i + 4]["content"]["$t"].split("|");
+                    var varListOfLocation = result["feed"]["entry"][i + 5]["content"]["$t"].split("@");
+                    for (var z=0; z < varListOfLocation.length; z++){
+                        arrayForMap.push({
+                            "title": result["feed"]["entry"][i]["content"]["$t"],
+                            "latitude": parseFloat((varListOfLocation[z].split(","))[0]),
+                            "longitude": parseFloat((varListOfLocation[z].split(","))[1]),
+                            "color":null
+                          });
+                    }
+                    for (var z = 0; z < totalSources.length; z++) {
+                        res += '<a rel="external nofollow" href="' + totalSources[z] + '">Source[' + (z + 1) + ']</a><br>';
+                    }
+                    table += res + '</td>' + '</tr>';
                 }
-                table += res + '</td>' + '</tr>';
+                document.getElementById("insertPhase1Here").innerHTML = table;
+                doPhase0();
             }
-            document.getElementById("insertPhase1Here").innerHTML = table;
-        }
-    });
+        });
+    }
 
     // Fetch Vaccine details for phase 0
-    $.ajax({
-        url: "https://spreadsheets.google.com/feeds/cells/1PINwxQbZr14xz_TfODu0_XQNRPuAlsGgIqcVbFjGOn8/1/public/values?alt=json",
-        success: function(result) {
-            var table = "";
-            for (var i = 0; i < result["feed"]["entry"].length - 1; i = i + 5) {
-                table += "<tr>";
-                table += '<td style="font-size: 0.8em;">' + result["feed"]["entry"][i]["content"]["$t"] + '</td>';
-                table += '<td style="font-size: 0.8em;">' + result["feed"]["entry"][i + 1]["content"]["$t"] + '</td>';
-                table += '<td style="font-size: 0.8em;background-color:#99B898;color: white;" class="disMobile"  >' + result["feed"]["entry"][i + 2]["content"]["$t"] + '</td>';
-                table += '<td style="font-size: 0.8em;">' + result["feed"]["entry"][i + 3]["content"]["$t"] + '</td>';
-                var res = "<td>";
-                var totalSources = result["feed"]["entry"][i + 4]["content"]["$t"].split("|");
-                for (var z = 0; z < totalSources.length; z++) {
-                    res += '<a rel="external nofollow" href="' + totalSources[z] + '">Source[' + (z + 1) + ']</a><br>';
+    function doPhase0(){
+        $.ajax({
+            url: "https://spreadsheets.google.com/feeds/cells/1VOgZcm9CawE-oskv-QSIeAd29-rHRj7CSYWWqwffE_0/1/public/values?alt=json",
+            success: function(result) {
+                var table = "";
+                for (var i = 0; i < result["feed"]["entry"].length - 1; i = i + 6) {
+                    table += "<tr>";
+                    table += '<td style="font-size: 0.8em;">' + result["feed"]["entry"][i]["content"]["$t"] + '</td>';
+                    table += '<td style="font-size: 0.8em;">' + result["feed"]["entry"][i + 1]["content"]["$t"] + '</td>';
+                    table += '<td style="font-size: 0.8em;background-color:#99B898;color: white;" class="disMobile"  >' + result["feed"]["entry"][i + 2]["content"]["$t"] + '</td>';
+                    table += '<td style="font-size: 0.8em;">' + result["feed"]["entry"][i + 3]["content"]["$t"] + '</td>';
+                    var res = "<td>";
+                    var totalSources = result["feed"]["entry"][i + 4]["content"]["$t"].split("|");
+    
+                    var varListOfLocation = result["feed"]["entry"][i + 5]["content"]["$t"].split("@");
+                    for (var z=0; z < varListOfLocation.length; z++){
+                        if (!isNaN(parseFloat((varListOfLocation[z].split(","))[0]))){
+                            arrayForMap.push({
+                                "title": result["feed"]["entry"][i]["content"]["$t"],
+                                "latitude": parseFloat((varListOfLocation[z].split(","))[0]),
+                                "longitude": parseFloat((varListOfLocation[z].split(","))[1]),
+                                "color":null
+                              });
+                        }
+                    }
+                    
+                    
+                      for (var z = 0; z < totalSources.length; z++) {
+                        res += '<a rel="external nofollow" href="' + totalSources[z] + '">Source[' + (z + 1) + ']</a><br>';
+                    }
+                    table += res + '</td>' + '</tr>';
                 }
-                table += res + '</td>' + '</tr>';
-            }
-            document.getElementById("insertPhase0Here").innerHTML = table;
-        }
-    });
+                document.getElementById("insertPhase0Here").innerHTML = table;
+    
+    
+                am4core.ready(function() {
+                    // Themes begin
+                    am4core.useTheme(am4themes_animated);
+                    // Themes end
+                    
+                    // Create map instance
+                    var chart = am4core.create("chartdiv", am4maps.MapChart);
+                    
+                    // Set map definition
+                    chart.geodata = am4geodata_worldLow;
+                    
+                    // Set projection
+                    chart.projection = new am4maps.projections.Miller();
+                    
+                    // Create map polygon series
+                    var polygonSeries = chart.series.push(new am4maps.MapPolygonSeries());
+                    
+                    // Exclude Antartica
+                    polygonSeries.exclude = ["AQ"];
+                    
+                    // Make map load polygon (like country names) data from GeoJSON
+                    polygonSeries.useGeodata = true;
+                    
+                    // Configure series
+                    var polygonTemplate = polygonSeries.mapPolygons.template;
+                    polygonTemplate.tooltipText = "{name}";
+                    polygonTemplate.polygon.fillOpacity = 0.6;
+                    
+                    
+                    // Create hover state and set alternative fill color
+                    var hs = polygonTemplate.states.create("hover");
+                    hs.properties.fill = chart.colors.getIndex(0);
+                    
+                    // Add image series
+                    var imageSeries = chart.series.push(new am4maps.MapImageSeries());
+                    imageSeries.mapImages.template.propertyFields.longitude = "longitude";
+                    imageSeries.mapImages.template.propertyFields.latitude = "latitude";
+                    imageSeries.mapImages.template.tooltipText = "{title}";
+                    imageSeries.mapImages.template.propertyFields.url = "url";
+                    
+                    var circle = imageSeries.mapImages.template.createChild(am4core.Circle);
+                    circle.radius = 3;
+                    circle.propertyFields.fill = "color";
+                    
+                    var circle2 = imageSeries.mapImages.template.createChild(am4core.Circle);
+                    circle2.radius = 3;
+                    circle2.propertyFields.fill = "color";
+                    
+                    
+                    circle2.events.on("inited", function(event){
+                      animateBullet(event.target);
+                    })
+                    
+                    
+                    function animateBullet(circle) {
+                        var animation = circle.animate([{ property: "scale", from: 1, to: 5 }, { property: "opacity", from: 1, to: 0 }], 1000, am4core.ease.circleOut);
+                        animation.events.on("animationended", function(event){
+                          animateBullet(event.target.object);
+                        })
+                    }
+                    
+                    var colorSet = new am4core.ColorSet();
+                    
+                    for (var z=0; z < arrayForMap.length; z++){
+                        arrayForMap[z]["color"] = colorSet.next();
+                    }
+            
+                    imageSeries.data = arrayForMap;
+                    
+                    });
+    
+    
+    
+    
+    
+    
+            }    
+        });
+
+    }
+
 })(jQuery);
